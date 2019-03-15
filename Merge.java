dcd8d94231e4cdc22dc8424763f28a235b5e199d
Jackson Zou <jzou00@stuy.edu>
2019-03-15 10:50:31 -0400

    wrote method for adding to new arrays

--- Merge.java
+++ Merge.java
@@ -5,6 +5,16 @@ public class Merge{
   }
 
   public static void mergesort(int[] data, int lo, int hi) {
-
+    if (lo >= hi) {
+      return;
+    }
+    int[] left = new int[(hi-lo)/2 + (hi-lo)%2];
+    int[] right = new int[(hi-lo)/2];
+    for (int i = 0; i < left.length; i++) {
+      left[i] = data[i];
+    }
+    for (int i = 0; i < right.length; i++) {
+      right[i] = data[i + left.length];
+    }
   }
 }
