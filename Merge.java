82249f85a30fc4bbc47003ace7577cf79d084aa2
Jackson Zou <jzou00@stuy.edu>
2019-03-15 11:01:36 -0400

    able to split down to array size 1

--- Merge.java
+++ Merge.java
@@ -8,6 +8,7 @@ public class Merge{
 
   public static void mergesort(int[] data, int lo, int hi) {
     if (lo >= hi) {
+      System.out.println(Arrays.toString(data));
       return;
     }
     int[] left = new int[(hi-lo+1)/2 + (hi-lo+1)%2];
@@ -18,12 +19,15 @@ public class Merge{
     for (int i = 0; i < right.length; i++) {
       right[i] = data[i + left.length];
     }
-    System.out.println(Arrays.toString(left));
-    System.out.println(Arrays.toString(right));
+    mergesort(left, lo, lo + left.length-1);
+    mergesort(right, lo + left.length, hi);
+
+    //System.out.println(Arrays.toString(left));
+    //System.out.println(Arrays.toString(right));
   }
 
   public static void main(String[] args) {
-    int[] ary = new int[] {1,3,4,5,11,4,4,5,2,4,2,4,6,7,7,212,3,54,5,1,2,8};
+    int[] ary = new int[] {1,3,4,5,11,4,4};
     System.out.println(Arrays.toString(ary));
     mergesort(ary);
   }
