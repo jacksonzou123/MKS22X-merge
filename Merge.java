706e9c4b2b8ac4bf422c767bfd27de2da1958e4e
Jackson Zou <jzou00@stuy.edu>
2019-03-15 10:46:22 -0400

    Created class and wrote method headers

---
+++ Merge.java
@@ -0,0 +1,10 @@
+public class Merge{
+  /*sort the array from least to greatest value. This is a wrapper function*/
+  public static void mergesort(int[]data){
+    mergesort(data, 0, data.length-1);
+  }
+
+  public static void mergesort(int[] data, int lo, int hi) {
+
+  }
+}
